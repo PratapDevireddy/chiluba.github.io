2c491e7cdfa374bb7ea72da4aba868e3a3a39989
Avinash Reddy Desireddy <avinashreddy1090@outlook.com>
2014-12-23 12:45:40 -0600

    Chiluba new repo push

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Created by awinash on 11/22/14.
+ */
+
+// THIS IS NAVIGATION.
+$(document).ready(function () {
+
+    var location;
+    var targetClass;
+    var navbarSelector = $('a.navigate');
+
+    targetClass = window.location.href.split('#')[1];
+
+    if (typeof targetClass == 'undefined') {
+        targetClass = "home";
+    }
+
+    $(".book, .home, .author, .contact").css('display', 'none');
+    $("." + targetClass).css('display', 'block');
+
+
+    $(navbarSelector).click(function () {
+        location = $(this).data("location");
+        $(this).attr({href: '#' + location});
+        var target = $(this).attr("href");
+        targetClass = "." + target.substring(1, target.length);
+        $(".book, .home, .author, .contact").css('display', 'none');
+        $(targetClass).css('display', 'block');
+    });
+});
